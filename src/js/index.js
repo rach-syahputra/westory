417aa98c25d238adfd41404612c5538515d912c4
rach syahputra <rach.syahputra@gmail.com>
2024-07-23 23:52:39 +0800

    implement page routing

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,27 @@ import './components/index'
 
 // Import javascript file as needed
 import * as bootstrap from 'bootstrap'
+
+// Routing
+const routes = {
+  '/': Dashboard
+}
+
+const detectRoute = () => routes[window.location.pathname]
+
+const initPages = () => {
+  const header = document.querySelector('header')
+  const main = document.querySelector('main')
+  const footer = document.querySelector('footer')
+
+  if (header && main && footer) {
+    main.style.minHeight = `calc(100vh - ${header.clientHeight + footer.clientHeight}px)`
+  }
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  initPages()
+
+  const route = detectRoute()
+  route.init()
+})
