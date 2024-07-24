1d798da8dcf43277cf83d36a18a1ede9ce2156d8
rach syahputra <rach.syahputra@gmail.com>
2024-07-24 18:25:42 +0800

    implement localization for nav-links component

--- src/js/components/bootstrap/NavLinks.js
+++ src/js/components/bootstrap/NavLinks.js
@@ -1,16 +1,18 @@
 import { html } from 'lit'
 import LitWithoutShadowDom from '../base/LitWithoutShadowDom'
+import { msg, updateWhenLocaleChanges } from '@lit/localize'
 
 class NavLinks extends LitWithoutShadowDom {
   constructor() {
     super()
+    updateWhenLocaleChanges(this)
   }
 
   render() {
     return html`
       <ul class="navbar-nav">
-        <nav-link to="/" content="Dashboard"></nav-link>
-        <nav-link to="/about.html" content="About"></nav-link>
+        <nav-link to="/" content="${msg(`Dashboard`)}"></nav-link>
+        <nav-link to="/about.html" content="${msg(`About`)}"></nav-link>
       </ul>
     `
   }
