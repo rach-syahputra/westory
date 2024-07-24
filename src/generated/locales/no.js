8a2c9919456d9e9d03b4a2491bacfdfa70c61f8d
rach syahputra <rach.syahputra@gmail.com>
2024-07-24 18:28:08 +0800

    build locale modules

---
+++ src/generated/locales/no.js
@@ -0,0 +1,18 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's0487f33d9ddaabde': ``,
+'s520770ef613930ce': `Velkommen til StoryApp, hvor historier kommer til live!`,
+'s5a42fe1f271dba1b': `legge til ny historie`,
+'s7fee95b66225e1fb': `Dashbord`,
+'sd262aafcbbbbd642': `En dedikert og entusiastisk "Front-End Web Development"-student med lidenskap for å lage visuelt tiltalende og brukervennlige webapplikasjoner.`,
+    };
+  
