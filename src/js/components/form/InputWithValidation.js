0fff797e44af07596bc007cf029616d8bcecca2c
rach syahputra <rach.syahputra@gmail.com>
2024-07-27 18:43:15 +0800

    create input-with-validation component

---
+++ src/js/components/form/InputWithValidation.js
@@ -0,0 +1,56 @@
+import { html, nothing } from 'lit'
+import LitWithoutShadowDom from '../base/LitWithoutShadowDom'
+
+class InputWithValidation extends LitWithoutShadowDom {
+  static properties = {
+    value: { type: String, reflect: true },
+    type: { type: String, reflect: true },
+    inputId: { type: String, reflect: true },
+
+    validFeedbackMessage: { type: String, reflect: true },
+    invalidFeedbackMessage: { type: String, reflect: true },
+
+    required: { type: Boolean, reflect: true },
+  }
+
+  constructor() {
+    super()
+    this._checkAvailabilityProperty()
+
+    this.required = false
+  }
+
+  _checkAvailabilityProperty() {
+    if (!this.hasAttribute('invalidFeedbackMessage')) {
+      throw new Error(
+        `Atribut "invalidFeedbackMessage" harus diterapkan pada elemen ${this.localName}`
+      )
+    }
+  }
+
+  render() {
+    return html`
+      <input
+        id=${this.inputId || nothing}
+        class="form-control"
+        type=${this.type}
+        value=${this.value || nothing}
+        ?required=${this.required}
+        @input=${(e) => (this.value = e.target.value)}
+      ></input>
+
+      ${this._validFeedbackTemplate()}
+      <div class="invalid-feedback">${this.invalidFeedbackMessage}</div>
+    `
+  }
+
+  _validFeedbackTemplate() {
+    if (this.validFeedbackMessage) {
+      return html` <div class="valid-feedback">${this.validFeedbackMessage}</div> `
+    }
+
+    return html``
+  }
+}
+
+customElements.define('input-with-validation', InputWithValidation)
