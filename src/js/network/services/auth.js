adc1854117de1ce2e540ae12556b919cabf66b0c
rach syahputra <rach.syahputra@gmail.com>
2024-07-27 19:34:10 +0800

    add login function for Auth network service

--- src/js/network/services/auth.js
+++ src/js/network/services/auth.js
@@ -1,5 +1,6 @@
 import { basicAxios } from '../instance'
 import API_ENDPOINTS from '../endpoint'
+import Utils from '../../utils/Utils'
 
 const Auth = {
   register: async ({ name, email, password }) => {
@@ -11,7 +12,23 @@ const Auth = {
       }
       const response = await basicAxios.post(API_ENDPOINTS.REGISTER, requestBody)
 
-      console.log(response)
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  login: async ({ email, password }) => {
+    try {
+      const requestBody = {
+        email,
+        password,
+      }
+      const response = await basicAxios.post(API_ENDPOINTS.LOGIN, requestBody)
+
+      if (!response?.data.error) {
+        Utils.setUserToken('token', response.data.loginResult.token)
+      }
 
       return response.data
     } catch (error) {
