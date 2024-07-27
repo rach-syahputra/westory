26b311340eabf3b613223860974dfa0a366e93cf
rach syahputra <rach.syahputra@gmail.com>
2024-07-27 15:44:35 +0800

    integrate prettier with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-];
+  eslintPluginPrettierRecommended
+]
