81df0a58b63928fbb4fd3723f96107059ba612a9
rach syahputra <rach.syahputra@gmail.com>
2024-07-28 19:17:59 +0800

    add addStory function in story network service

--- src/js/network/services/story.js
+++ src/js/network/services/story.js
@@ -3,7 +3,15 @@ import API_ENDPOINTS from '../endpoint'
 
 const Story = {
   getAllStories: async () => {
-    return await authAxios.get(API_ENDPOINTS.GET_ALL_STORIES)
+    return await authAxios.get(API_ENDPOINTS.STORIES)
+  },
+
+  addStory: async ({ description, photo }) => {
+    const requestBody = {
+      description,
+      photo,
+    }
+    return await authAxios.post(API_ENDPOINTS.STORIES, requestBody)
   },
 }
 
